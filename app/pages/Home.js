6d090de84bcfce78d85a17b6dacec8c1dce4449c
Yohanna Fransiska Aladina <42995520+yohannafrans@users.noreply.github.com>
2020-07-01 18:22:55 +0700

    Edit Home.js

--- app/pages/Home.js
+++ app/pages/Home.js
@@ -9,39 +9,69 @@ import {
 const Home = () => {
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={() => console.log('Device 1')}>
-              <View style={styles.button}>
-                <Text style={styles.buttonText}>Device 1</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => console.log('Device 2')}>
-              <View style={styles.button}>
-                <Text style={styles.buttonText}>Device 2</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => console.log('Device 3')}>
-              <View style={styles.button}>
-                <Text style={styles.buttonText}>Device 3</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => console.log('Device 4')}>
-              <View style={styles.button}>
-                <Text style={styles.buttonText}>Device 4</Text>
-              </View>
-            </TouchableOpacity>
+            <View style={{paddingTop:20, flexDirection:'row', justifyContent:'space-between'}}>
+              <TouchableOpacity onPress={() => console.log('Device 1')}>
+                <View style={styles.buttonTop}>
+                  <Text style={styles.buttonTopText}>Device 1</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => console.log('Device 2')}>
+                <View style={styles.buttonTop}>
+                  <Text style={styles.buttonTopText}>Device 2</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => console.log('Device 3')}>
+                <View style={styles.buttonTop}>
+                  <Text style={styles.buttonTopText}>Device 3</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => console.log('Device 4')}>
+                <View style={styles.buttonTop}>
+                  <Text style={styles.buttonTopText}>Device 4</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            
+            <View style={styles.lineStyle}/>
+            <View style={{paddingTop:20, flexDirection:'column', paddingRight: 20}}>
+              <TouchableOpacity onPress={() => console.log('pH')}>
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>pH</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => console.log('Suhu')}>
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>Suhu</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => console.log('Kelembaban')}>
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>Kelembaban</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => console.log('Kedalaman')}>
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>Kedalaman</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+
         </View>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 20,
-        flexDirection: 'row',
-        justifyContent:'space-between'
+      alignItems: 'stretch'
     },
 
-    button: {
-        marginBottom: 30,
+    lineStyle: {
+      borderWidth: 1,
+      borderColor: '#56C9FB',
+    },
+
+    buttonTop: {
+        marginBottom: 20,
         width: 80,
         height: 80,
         alignItems: 'center',
@@ -50,11 +80,29 @@ const styles = StyleSheet.create({
         borderRadius:40
     },
 
-    buttonText: {
+    buttonTopText: {
       textAlign: 'center',
       fontSize: 12,
       padding: 10,
       color: 'white'
+    },
+
+    button: {
+      marginBottom: 30,
+      borderColor: '#56C9FB',
+      borderWidth: 2,
+      height: 120,
+      width: 120,
+      alignItems: 'center',
+      justifyContent:'center',
+      backgroundColor:'white'
+    },
+
+    buttonText: {
+      textAlign: 'center',
+      fontSize: 16,
+      padding: 10,
+      color: '#56C9FB'
     }
 });
 
