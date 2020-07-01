d1d4bba6d8b1e0e943808327c1c342325e7d1a84
rizalbayyu <38995446+rizalbayyu@users.noreply.github.com>
2020-07-01 12:27:34 +0700

    Update TabDevice
    Edit margin Top

--- app/pages/TampilanDevice.js
+++ app/pages/TampilanDevice.js
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
     },
 
     button: {
-        marginTop: 150,
+        marginTop: 30,
         marginBottom: 30,
         width: 300,
         alignItems: 'center',
